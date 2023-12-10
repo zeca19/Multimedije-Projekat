d873ed682a19841e8d3691e81b163cbd24d991ce
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-12-10 03:00:34 +0100

    Ispravljene neke greske, refaktorisan kod i promenjena struktura, ps dodate slike za aliene

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from player import Player
 import obstacle
-
+from alien import Alien
 class Game:
 
     def __init__(self):
@@ -17,6 +17,10 @@ class Game:
         self.obstacle_x_positions=[num*(screen_width/self.obstacle_amount) for  num in range(self.obstacle_amount)]
         self.create_multiple_obstacles(*self.obstacle_x_positions,x_start=screen_width/15,y_start=480)
 
+
+        # Alien setup
+        self.aliens=pygame.sprite.Group()
+        self.alien_setup(1,1)
     
     def create_obstacle(self,x_start,y_start,offset_x):
         for row_index, row in enumerate(self.shape):
@@ -31,11 +35,20 @@ class Game:
         for x in offset:
             self.create_obstacle(x_start,y_start,x)
 
+    def alien_setup(self,rows,cols,x_distance=60,y_distance=48):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x=col_index
+                y=row_index
+                alien_sprite=Alien('red',x,y)
+                self.aliens.add(alien_sprite)
+
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
         # Treba da azurira sve grupe
         # I treba da nacrta sve grupe
         
