321353ef30492f3f347048358622bd3dc06d82a5
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-12-10 01:03:45 +0100

    Dodata opcija za pucanje ostala je animacija naravno ali smo namestili da moze na 6 sekundi da se puca

--- player.py
+++ player.py
@@ -9,6 +9,9 @@ class Player(pygame.sprite.Sprite):
         self.rect=self.image.get_rect(midbottom=pos)
         self.speed=speed
         self.max_x_constraint=constraint
+        self.laser_time=0
+        self.ready=True
+        self.laser_coldown=600
 
     # Sluzi za gledanje koje je dugme pritisnuto
     def get_input(self):
@@ -19,14 +22,31 @@ class Player(pygame.sprite.Sprite):
         elif keys[pygame.K_LEFT]:
             self.rect.x-=self.speed
 
+        if keys[pygame.K_SPACE] and self.ready:
+            self.shoot_laser()
+            self.ready=False
+            self.laser_time=pygame.time.get_ticks()
+
+    
+    def recharge(self):
+        if not self.ready:
+            current_time=pygame.time.get_ticks()
+            if current_time-self.laser_time>=self.laser_coldown:
+                self.ready=True
+
     
     def update(self):
         self.get_input()
         self.constraint()
+        self.recharge()
 
     def constraint(self):
         if self.rect.left<=0:
             self.rect.left=0
         if self.rect.right>=self.max_x_constraint:
             self.rect.right=self.max_x_constraint
+
+
+    def shoot_laser(self):
+        print("Pucamo sad")
        
